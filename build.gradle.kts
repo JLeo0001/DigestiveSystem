7b4824d93e41dbc517104d91c3c4ea8342c4df0a
JLeo0001 <104238511+JLeo0001@users.noreply.github.com>
2025-12-05 15:02:29 +0800

    Initialize build.gradle.kts with PaperMC setup

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    java
+    id("io.papermc.paper.weight.userdev") version "1.7.1"
+}
+
+group = "com.example"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    paperweight.paperDevBundle("1.21-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.assemble {
+    dependsOn(tasks.reobfJar)
+}
